51dbff5f51c81a9abe8966dd6a20c1eb4b460f41
appledev201888 <2779461804@qq.com>
2019-03-30 15:59:20 +0800

    提交的描述说明

--- Classes/CJZAToolKits.framework/Headers/CJZAToolKits.h
+++ Classes/CJZAToolKits.framework/Headers/CJZAToolKits.h
@@ -1,14 +1,14 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for IQKeyboardTools.
-FOUNDATION_EXPORT double IQKeyboardToolsVersionNumber;
+//! Project version number for CJZToolKits.
+FOUNDATION_EXPORT double CJZToolKitsVersionNumber;
 
-//! Project version string for IQKeyboardTools.
-FOUNDATION_EXPORT const unsigned char IQKeyboardToolsVersionString[];
+//! Project version string for CJZToolKits.
+FOUNDATION_EXPORT const unsigned char CJZToolKitsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <IQKeyboardTools/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <CJZToolKits/PublicHeader.h>
 
-#import <IQKeyboardTools/IQKeyboardToolsManage.h>
+#import <CJZToolKits/SVProgressHUDExtManage.h>
 
 
